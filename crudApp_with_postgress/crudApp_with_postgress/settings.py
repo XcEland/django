ae907d4e5ba5c35e69331c38b8dca5de58c08762
XcEland <mkoma.xyz@gmail.com>
2024-05-06 20:55:11 +0200

    railway

--- crudApp_with_postgress/crudApp_with_postgress/settings.py
+++ crudApp_with_postgress/crudApp_with_postgress/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'crudApp_with_postgress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CzqtYqbzMqsJiTmhdsPgOaNgWNoVaOTZ',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '25409',
     }
 }
 
